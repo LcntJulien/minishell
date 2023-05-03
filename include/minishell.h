e94fc5bfea9fcd9ddcb4dfa93ea4f80930b7cf2c
Julio <73828002+LcntJulien@users.noreply.github.com>
2023-05-03 15:53:22 +0200

    Update minishell.h
    Base has been written, including libft and some other includes, it will now be completed commit by commit

--- include/minishell.h
+++ include/minishell.h
@@ -6,7 +6,21 @@
 /*   By: jlecorne <jlecorne@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/03 14:06:16 by jlecorne          #+#    #+#             */
-/*   Updated: 2023/05/03 14:06:17 by jlecorne         ###   ########.fr       */
+/*   Updated: 2023/05/03 15:50:26 by jlecorne         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# ifndef LIBFT
+#  define LIBFT "../libft/libft.h"
+# endif
+
+# include LIBFT
+# include <stdio.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <string.h>
+
+#endif
