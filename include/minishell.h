ce5f2a63d29d27b7dfcecba0c0e700a2bcce7d57
Julio <73828002+LcntJulien@users.noreply.github.com>
2023-05-06 16:06:20 +0200

    Update minishell.h
    Added includes for the readline function and a struct shell

--- include/minishell.h
+++ include/minishell.h
@@ -6,7 +6,7 @@
 /*   By: jlecorne <jlecorne@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/03 14:06:16 by jlecorne          #+#    #+#             */
-/*   Updated: 2023/05/04 20:55:50 by jlecorne         ###   ########.fr       */
+/*   Updated: 2023/05/06 15:42:35 by jlecorne         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,21 @@
 #  define LIBFT "../libft/include/libft.h"
 # endif
 
+typedef struct s_shell
+{
+	char	*input;
+	int		inf;
+	int		outf;
+	int		rtn;
+	int		pid;
+}			t_shell;
+
 # include LIBFT
+# include <readline/history.h>
+# include <readline/readline.h>
 # include <stdio.h>
-# include <unistd.h>
 # include <stdlib.h>
 # include <string.h>
+# include <unistd.h>
 
 #endif
