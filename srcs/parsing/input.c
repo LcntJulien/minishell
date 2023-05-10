1bf46146173248140734a2a29a8c5b66b7eeb861
Julio <73828002+LcntJulien@users.noreply.github.com>
2023-05-10 16:09:59 +0200

    Update input.c

--- srcs/parsing/input.c
+++ srcs/parsing/input.c
@@ -6,7 +6,7 @@
 /*   By: jlecorne <jlecorne@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/09 15:37:14 by jlecorne          #+#    #+#             */
-/*   Updated: 2023/05/10 12:01:46 by jlecorne         ###   ########.fr       */
+/*   Updated: 2023/05/10 15:43:41 by jlecorne         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,9 @@
 
 void	parse(t_shell *mini)
 {
-	char *line;
-	t_token *token;
+	// char *line;
+	// t_token *token;
 
     ft_putstr_fd((*mini).input, 1);
+    ft_putchar_fd('\n', 1);
 }
