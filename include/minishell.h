32d043e7d08677c0207a75686a4b090a3437c116
Julio <73828002+LcntJulien@users.noreply.github.com>
2023-05-10 14:22:17 +0200

    Update minishell.h

--- include/minishell.h
+++ include/minishell.h
@@ -6,47 +6,53 @@
 /*   By: jlecorne <jlecorne@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/03 14:06:16 by jlecorne          #+#    #+#             */
-/*   Updated: 2023/05/09 21:53:01 by jlecorne         ###   ########.fr       */
+/*   Updated: 2023/05/10 14:20:47 by jlecorne         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
-# ifndef LIBFT
-#  define LIBFT "../libft/include/libft.h"
-# endif
+// # ifndef LIBFT
+// #  define LIBFT "libft.h"
+// # endif
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+#include <signal.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <dirent.h>
+#include <fcntl.h>
+#include <termios.h>
+#include <sys/ioctl.h>
+#include <curses.h>
+#include <readline/history.h>
+#include <readline/readline.h>
+
+#include "../libft/include/libft.h"
 
 typedef struct s_shell
 {
-	char	*input;
-	int		in;
-	int		out;
-	int		rtn;
-	int		pid;
-	int		exit;
-}			t_shell;
+	char			*input;
+	int				in;
+	int				out;
+	int				rtn;
+	int				pid;
+	int				exit;
+}					t_shell;
 
 typedef struct s_token
 {
-	char	*s;
-	int		type;
-	t_token	*prev;
-	t_token	*next;
-}			t_token;
+	char			*s;
+	int				type;
+	struct s_token	*prev;
+	struct s_token	*next;
+}					t_token;
 
-# include LIBFT
-# include <dirent.h>
-# include <errno.h>
-# include <fcntl.h>
-# include <limits.h>
-# include <readline/history.h>
-# include <readline/readline.h>
-# include <signal.h>
-# include <stdio.h>
-# include <stdlib.h>
-# include <string.h>
-# include <sys/wait.h>
-# include <unistd.h>
+void				parse(t_shell *mini);
 
 #endif
