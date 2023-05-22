18cceb7058f271ede941281ee1ad128f4db9e052
Julio <73828002+LcntJulien@users.noreply.github.com>
2023-05-22 19:47:33 +0200

    Update main.c
    Added sigint handler to manage to quit process while exit command is not set

--- srcs/main.c
+++ srcs/main.c
@@ -6,29 +6,34 @@
 /*   By: jlecorne <jlecorne@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/03 14:07:16 by jlecorne          #+#    #+#             */
-/*   Updated: 2023/05/11 15:49:48 by jlecorne         ###   ########.fr       */
+/*   Updated: 2023/05/22 19:46:33 by jlecorne         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
 
+void	sigint_handler(int sig)
+{
+	ft_putstr_fd("\033[0;35m\033[1mminishell ▸ \033[0m", 2);
+	printf("Received SIGINT signal (%d)\n", sig);
+	system("leaks minishell");
+	exit(0);
+}
+
 int	main(void)
 {
 	t_shell	mini;
-	char	*excmd;
 
-	excmd = "exit";
-	mini.in = dup(STDIN_FILENO);
-	mini.out = dup(STDOUT_FILENO);
+	// mini.in = dup(STDIN_FILENO);
+	// mini.out = dup(STDOUT_FILENO);
 	mini.rtn = 0;
 	mini.exit = 0;
+	signal(SIGINT, sigint_handler);
 	while (mini.exit == 0)
 	{
-		mini.input = readline("\033[0;35m\033[1mminishell ▸ \033[0m");
-		if (ft_strncmp(excmd, mini.input, ft_strlen(mini.input)) == 0)
-			mini.exit = 1;
+		mini.line = readline("\033[0;35m\033[1mminishell ▸ \033[0m");
 		parse(&mini);
 	}
-	free(mini.input);
+	listfree(mini.token);
 	return (0);
 }
