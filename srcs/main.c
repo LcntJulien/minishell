ef37f13451b4e13a0115f74d00dcdfd75d548741
Julio <73828002+LcntJulien@users.noreply.github.com>
2023-05-25 12:27:55 +0200

    Update main.c

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: jlecorne <jlecorne@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/03 14:07:16 by jlecorne          #+#    #+#             */
-/*   Updated: 2023/05/25 12:22:50 by jlecorne         ###   ########.fr       */
+/*   Updated: 2023/05/25 12:25:57 by jlecorne         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,17 +20,17 @@ void	sigint_handler(int sig)
 	exit(0);
 }
 
-// static void	startshell(t_shell *mini)
-// {
-// 	mini->in = dup(STDIN_FILENO);
-// 	mini->out = dup(STDOUT_FILENO);
-// 	mini->rtn = 0;
-// 	mini->exit = 0;
-// copy_env(mini, env);
-	// find_pwd(mini);
-// }
+static void	startshell(t_shell *mini, char	**env)
+{
+	mini->in = dup(STDIN_FILENO);
+	mini->out = dup(STDOUT_FILENO);
+	mini->rtn = 0;
+	mini->exit = 0;
+	copy_env(mini, env);
+	find_pwd(mini);
+}
 
-static void	args(int argc, char **argv)
+static void	args(int ac, char **av)
 {
 	(void)av;
 	if (ac < 1 || ac > 1)
@@ -44,12 +44,10 @@ int	main(int ac, char **av, char **env)
 {
 	t_shell	mini;
 
-	args(argc, argv);
-	startshell(&mini);
-	ft_env(&mini, envp);
+	args(ac, av);
+	startshell(&mini, env);
+	ft_env(&mini, env);
 
-	// mini.in = dup(STDIN_FILENO);
-	// mini.out = dup(STDOUT_FILENO);
 	mini.rtn = 0;
 	mini.exit = 0;
 	signal(SIGINT, sigint_handler);
