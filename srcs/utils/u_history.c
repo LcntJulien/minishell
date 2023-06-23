e42e94eb9563be34c7c838cffc02b59432dd0b99
jmathieu <jmathieu@DESKTOP-V53NCP0>
2023-06-23 09:03:17 +0200

    test on histo

--- srcs/utils/u_history.c
+++ srcs/utils/u_history.c
@@ -6,7 +6,7 @@
 /*   By: jmathieu <jmathieu@student.42mulhouse.fr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/21 16:17:11 by jmathieu          #+#    #+#             */
-/*   Updated: 2023/06/22 19:03:05 by jmathieu         ###   ########.fr       */
+/*   Updated: 2023/06/23 09:02:31 by jmathieu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,10 +21,29 @@ void	add_histo(char *str, int histo)
 	write(histo, "\n", 1);
 }
 
+static void	init_history(int *histo)
+{
+	char	*str;
+	char	*s;
+
+	str = get_next_line(*histo);
+	if (!str)
+		return ;
+	while (str)
+	{	
+		s = ft_substr(str, 0, ft_strlen(str) - 1);
+		add_history(s);
+		free(str);
+		free(s);
+		str = get_next_line(*histo);
+	}
+}
+
 int	create_history(int *histo)
 {
 	*histo = open(".mini_history", O_RDWR | O_APPEND | O_CREAT, 0644);
 	if (*histo == -1)
 		return (0);
+	init_history(histo);
 	return (1);
 }
