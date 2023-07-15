ef7b5af9b2aee57f095f2245d30e19cb14215f7e
Julio <73828002+LcntJulien@users.noreply.github.com>
2023-07-15 13:23:32 +0200

    Update u_signals.c

--- srcs/utils/u_signals.c
+++ srcs/utils/u_signals.c
@@ -3,10 +3,14 @@
 /*                                                        :::      ::::::::   */
 /*   u_signals.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jmathieu <jmathieu@student.42mulhouse.fr>  +#+  +:+       +#+        */
+/*   By: jlecorne <jlecorne@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/05 18:00:39 by jmathieu          #+#    #+#             */
+<<<<<<< Updated upstream
 /*   Updated: 2023/07/14 13:41:52 by jmathieu         ###   ########.fr       */
+=======
+/*   Updated: 2023/07/13 15:22:54 by jlecorne         ###   ########.fr       */
+>>>>>>> Stashed changes
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +18,7 @@
 
 static void	sigint_chose_handler(int s)
 {
+<<<<<<< Updated upstream
 	(void)s;
 	if (def_sig == 0)
 		sigint_0_handler();
@@ -27,6 +32,15 @@ static void	sigint_chose_handler(int s)
 		//sigint_4_handler();
 	//else if (def_sig == 1)
 		//sigint_5_handler();
+=======
+	(void)sig;
+	rl_redisplay();
+	ft_putstr_fd("\n", STDOUT_FILENO);
+	// rl_replace_line("", 0);
+	rl_on_new_line();
+	if (define_signal == FALSE)
+		rl_redisplay();
+>>>>>>> Stashed changes
 }
 
 //static void	siquit_handler(int s)
