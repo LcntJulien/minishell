07d84d0c4ce37833e9b3798ca543ee11be15d275
Julio <73828002+LcntJulien@users.noreply.github.com>
2023-07-15 13:22:02 +0200

    Create redir1.c

---
+++ srcs/exec/redir1.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   redir1.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jlecorne <jlecorne@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/15 12:57:43 by jlecorne          #+#    #+#             */
+/*   Updated: 2023/07/15 13:21:06 by jlecorne         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../include/minishell.h"
+
+// is_heredoc(t_shell *mini)
+// {
+// 	t_token	*tk;
+
+// 	tk = mini->token;
+//     while ()
+//     {
+//         /* code */
+//     }
+    
+// }
+
+// void	heredoc_manager(t_shell *mini)
+// {
+// 	if (!is_heredoc(mini))
+// 		return ;
+// }
