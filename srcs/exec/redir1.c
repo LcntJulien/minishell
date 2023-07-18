841b24c037cfea0024e89642e770dc7ad08cc817
Julio <73828002+LcntJulien@users.noreply.github.com>
2023-07-18 02:37:05 +0200

    Update redir1.c
    heredoc is now managed, still have to finish it so it can properly be redirected and handled

--- srcs/exec/redir1.c
+++ srcs/exec/redir1.c
@@ -6,26 +6,54 @@
 /*   By: jlecorne <jlecorne@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/15 12:57:43 by jlecorne          #+#    #+#             */
-/*   Updated: 2023/07/15 13:34:43 by jlecorne         ###   ########.fr       */
+/*   Updated: 2023/07/18 02:32:16 by jlecorne         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
 
-int	is_heredoc(t_shell *mini)
+void	heredoc_handler(t_shell *mini, t_token *tk)
 {
-	// t_token	*tk;
-	// tk = mini->token;
-	// while ()
-	// {
-	//     /* code */
-	// }
+	char	**tab;
+	char	**alloctab;
+	char	*tmp;
+	int		i;
+
 	(void)mini;
-    return (0);
+	tab = ft_calloc(sizeof(char *), 20);
+	alloctab = NULL;
+	tmp = NULL;
+	i = 0;
+	while (1)
+	{
+		tmp = readline("\033[0;35m\033[▸ \033[");
+		if (ft_strncmp(tmp, tk->s, ft_strlen(tmp)) == 0)
+			break ;
+		tab[i] = tmp;
+		i++;
+	}
+	alloctab = malloc(sizeof(char *) * i + 1);
+	if (!alloctab)
+		return ;
+	i = -1;
+	while (tab[++i] != NULL)
+		alloctab[i] = ft_strdup(tab[i]);
+	alloctab[i] = NULL;
 }
+
+	// i = -1;
+	// while (alloctab[++i] != NULL)
+	// 	fprintf(stderr, "%s\n", alloctab[i]);
 
 void	heredoc_manager(t_shell *mini)
 {
-	if (!is_heredoc(mini))
-		return ;
+	t_token *tk;
+
+	tk = mini->token;
+	while (tk->next != NULL)
+	{
+		if (tk->type == HEREDOC)
+			heredoc_handler(mini, tk->next);
+		tk = tk->next;
+	}
 }
