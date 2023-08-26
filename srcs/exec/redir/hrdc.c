e8221ca866cec86e00ec821b4a47d773fcee7341
Julio <73828002+LcntJulien@users.noreply.github.com>
2023-08-26 21:17:32 +0200

    Update hrdc.c

--- srcs/exec/redir/hrdc.c
+++ srcs/exec/redir/hrdc.c
@@ -6,15 +6,10 @@
 /*   By: jlecorne <jlecorne@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/15 12:57:43 by jlecorne          #+#    #+#             */
-<<<<<<< Updated upstream
-/*   Updated: 2023/08/26 21:15:36 by jlecorne         ###   ########.fr       */
-=======
-/*   Updated: 2023/08/26 21:13:32 by jlecorne         ###   ########.fr       */
->>>>>>> Stashed changes
+/*   Updated: 2023/08/26 21:17:17 by jlecorne         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../../../include/minishell.h"
 
 void	add_hrdc(t_shell *mini, t_hrdc *hrdc)
 {
